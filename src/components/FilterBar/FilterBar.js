c69964e585e7a6d7b2f940cf7bb1169dda7a6924
colin5530 <colinmcdermott90@gmail.com>
2021-06-02 16:35:17 +0100

    Adding slider component

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Slider } from 'antd';
 import { useUserData } from '../../userDataContext';
+import './FilterBar.css';
 
 const FilterBar = () => {
+  const className = 'c-FilterBar';
   const {visibleUsers, setVisibleUsers} = useUserData();
+  const [sliderValue, setSliderValue] = useState(500);
 
   return (
-    <div>
-      Filters
+    <div className={`${className}`}>
+      <div className={`${className}-slider-container`}>
+        <Slider
+          range
+          defaultValue={[500, 5000]}
+          min={0}
+          max={5000}
+          onChange={(val) => {
+            setSliderValue(val);
+          }}
+        />
+      </div>
+      <div className={`${className}-region-container`}>
+        Region
+      </div>
+      <div className={`${className}-gender-container`}>
+        Gender
+      </div>
     </div>
   )
 };
